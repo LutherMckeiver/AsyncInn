b61b4d87f81d5c788c968f262c7495b32a34749b
Luther Mckeiver <luthermckeiver@gmail.com>
2019-02-04 13:49:30 -0800

    Scaffolding and database are added.

---
+++ AsyncInn/AsyncInn/Migrations/20190204213929_initial.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AsyncInn.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Amenities",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Amenities", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Hotel",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hotel", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Room",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Layout = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Room", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HotelRoom",
+                columns: table => new
+                {
+                    HotelID = table.Column<int>(nullable: false),
+                    RoomID = table.Column<decimal>(nullable: false),
+                    Rate = table.Column<decimal>(nullable: false),
+                    Pets = table.Column<bool>(nullable: false),
+                    RoomID1 = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HotelRoom", x => new { x.HotelID, x.RoomID });
+                    table.ForeignKey(
+                        name: "FK_HotelRoom_Hotel_HotelID",
+                        column: x => x.HotelID,
+                        principalTable: "Hotel",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_HotelRoom_Room_RoomID1",
+                        column: x => x.RoomID1,
+                        principalTable: "Room",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RoomAmenities",
+                columns: table => new
+                {
+                    RoomID = table.Column<int>(nullable: false),
+                    AmenitiesID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoomAmenities", x => new { x.RoomID, x.AmenitiesID });
+                    table.ForeignKey(
+                        name: "FK_RoomAmenities_Amenities_AmenitiesID",
+                        column: x => x.AmenitiesID,
+                        principalTable: "Amenities",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RoomAmenities_Room_RoomID",
+                        column: x => x.RoomID,
+                        principalTable: "Room",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HotelRoom_RoomID1",
+                table: "HotelRoom",
+                column: "RoomID1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoomAmenities_AmenitiesID",
+                table: "RoomAmenities",
+                column: "AmenitiesID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HotelRoom");
+
+            migrationBuilder.DropTable(
+                name: "RoomAmenities");
+
+            migrationBuilder.DropTable(
+                name: "Hotel");
+
+            migrationBuilder.DropTable(
+                name: "Amenities");
+
+            migrationBuilder.DropTable(
+                name: "Room");
+        }
+    }
+}
---
+++ AsyncInn/AsyncInn/Models/HotelRoom.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Models
+{
+    public class HotelRoom
+    {
+        public int HotelID { get; set; }
+
+        public decimal RoomID { get; set; }
+
+        public decimal Rate { get; set; }
+
+        public bool  Pets { get; set; }
+
+        // Navigation Properties
+
+        public Hotel Hotel { get; set; }
+
+        public Room Room { get; set; }
+
+    }
+}
